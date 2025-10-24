89bc18ea99108e61b9aa141373a6226b8c763a94
nael <elioussoufinael@gmail.com>
2025-10-24 08:49:29 +0000

    0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * main - Entry point
  *
@@ -9,7 +10,7 @@ int main(void)
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
-	_putchar('t'1);
+	_putchar('t');
 	_putchar('c');
 	_putchar('h');
 	_putchar('a');
