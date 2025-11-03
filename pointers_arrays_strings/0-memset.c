9ba9119103699f2c22c6654645d7e431a233b35c
nael <duaboubakar@gmail.com>
2025-11-03 13:44:40 +0100

    0

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,13 +1,18 @@
 #include "main.h"
 
 /**
- * _memset 
+ * _memset - fills memory with a byte
+ * @s: memory
+ * @b: byte
+ * @n: size
+ * Return: s
  */
-
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
+
 	for (i = 0; i < n; i++)
 		s[i] = b;
+
 	return (s);
 }
