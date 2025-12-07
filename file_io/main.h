096214a162bc32022e08f171f0cc31107b4028ff
nael <duaboubakar@gmail.com>
2025-12-07 21:49:40 +0100

    3 fix

--- file_io/main.h
+++ file_io/main.h
@@ -6,9 +6,12 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-int _putchar(char c);
+/* prototypes des fonctions du projet file_io */
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+
+/* exigé par le sujet, même si tu ne l’utilises pas ici */
+int _putchar(char c);
 
 #endif /* MAIN_H */
